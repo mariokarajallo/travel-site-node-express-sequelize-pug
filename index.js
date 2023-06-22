f9985d1f27afa0705ddb7f7e5e37449d8019d093
mariokarajallo <mariokarajallo92@gmail.com>
2023-06-22 00:26:48 -0400

    feat: add rutes for page inicio, contacto, nosotros

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const app = express();
 //definir puerto
 const port = process.env.PORT || 4000;
 
-app.listen(() => {
+app.get("/", (req, res) => {
+  res.send("Inicio");
+});
+
+app.get("/contacto", (req, res) => {
+  res.send("Contacto");
+});
+
+app.get("/nosotros", (req, res) => {
+  res.send("Nosotros");
+});
+
+app.listen(port, () => {
   console.log(` el servidor se esta ejecutando en el puerto ${port}`);
 });
