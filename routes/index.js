febfd680964a25b69ec0de899740c48443632759
mariokarajallo <mariokarajallo92@gmail.com>
2023-06-22 23:58:14 -0400

    feat: create fodler layout and index.pug, inicio.pug

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Inicio");
+  res.render("inicio");
 });
 
 router.get("/contacto", (req, res) => {
