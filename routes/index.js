ebb197db1679154ae049dc2f5b66d0f195f18369
mariokarajallo <mariokarajallo92@gmail.com>
2023-06-23 23:15:31 -0400

    feat: create header.pug

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,6 @@ router.get("/", (req, res) => {
   res.render("inicio");
 });
 
-router.get("/contacto", (req, res) => {
-  res.send("Contacto");
-});
-
 router.get("/nosotros", (req, res) => {
   res.render("nosotros");
 });
