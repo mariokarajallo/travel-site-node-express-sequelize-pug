93911ec7386bb2670f3cb05e22bd188dbb546938
mariokarajallo <mariokarajallo92@gmail.com>
2023-06-29 00:28:12 -0400

    feat: add validation method guardarTestimonial

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -1,5 +1,32 @@
 const guardarTestimonial = (req, res) => {
-  console.log(req.body);
+  //validar formulario
+
+  const { nombre, correo, mensaje } = req.body;
+
+  const errores = [];
+
+  if (nombre.trim() === "") {
+    errores.push({ mensaje: "El nombre esta vacio" });
+  }
+  if (correo.trim() === "") {
+    errores.push({ mensaje: "El correo esta vacio" });
+  }
+  if (mensaje.trim() === "") {
+    errores.push({ mesanje: "El mensaje esta vacio" });
+  }
+
+  if (errores.length > 0) {
+    //mostrar la vista con errores
+    res.render("testimoniales", {
+      pagina: "Testimoniales",
+      errores,
+      nombre,
+      correo,
+      mensaje,
+    });
+  } else {
+    // almacenar datos del formulario en la base de datos
+  }
 };
 
 export { guardarTestimonial };
