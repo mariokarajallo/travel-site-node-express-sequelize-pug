4764472c7b8e7bed70ea4959fdbe066a385a6249
mariokarajallo <mariokarajallo92@gmail.com>
2023-06-30 22:42:12 -0400

    feat: add enviroment local

--- config/db.js
+++ config/db.js
@@ -1,15 +1,13 @@
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-
-dotenv.config();
+import Sequelize from "sequelize";
+import dotenv from "dotenv/config";
 
 const db = new Sequelize(
-  process.env.PLANETSCALE_DB,
-  process.env.PLANETSCALE_DB_USERNAME,
-  process.env.PLANETSCALE_DB_PASSWORD,
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
-    host: process.env.PLANETSCALE_DB_HOST,
-    port: "3306",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
     define: {
       timestamps: false,
